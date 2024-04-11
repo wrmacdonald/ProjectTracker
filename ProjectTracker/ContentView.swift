8a1ede1fdad8e12d123fe4f6655d784b514516c9
Wes MacDonald <wrmacdonald2016@gmail.com>
2024-04-11 12:14:10 -0700

    Initial Commit

---
+++ ProjectTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectTracker
+//
+//  Created by Wes MacDonald on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectTracker/ProjectTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTrackerApp.swift
+//  ProjectTracker
+//
+//  Created by Wes MacDonald on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
